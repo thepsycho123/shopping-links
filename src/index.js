87dfdb36afb8b2509d1646787adf0d10e2c3a9e9
Dinesh <dineshtogare@gmail.com>
2023-09-20 21:34:02 +0530

    final code
    final code shopping links

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const path = require("path");
+const hbs = require("hbs");
+const shop_user = require("./mongo");
+const port = process.env.PORT || 8080;
+app.use(express.json());
+
+dotenv.config();
+
+app.use(express.urlencoded({ extended: false }));
+
+const templatePath = path.join(__dirname, "../templates");
+app.set("view engine", "hbs");
+app.set("views", templatePath);
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+// Login User
+
+app.post("/login", async (req, res) => {
+  const data = {
+    name: req.body.name,
+    password: req.body.password,
+  };
+
+  await shop_user.insertMany([data]);
+
+  res.render("home");
+});
+
+//  Sign Up User
+
+app.post("/signup", async (req, res) => {
+  const data = {
+    name: req.body.name,
+    password: req.body.password,
+  };
+
+  await shop_user.insertMany([data]);
+
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log("port connected");
+});
---
+++ src/mongo.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://dineshtogare:dinesh123@shopping-app.uprdddq.mongodb.net/LoginSignupFacebook"
+  )
+
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch(() => {
+    console.log("failed to connected");
+  });
+
+const LogInSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const shop_user = new mongoose.model("shop_user", LogInSchema);
+
+module.exports = shop_user;
